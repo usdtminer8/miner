5a6a3f4709c2a0d5eae8d5d273cf75b9908d83d7
Ferdinand Weinberger <davorbrajevic@gmail.com>
2021-10-12 09:53:25 +0200

    Update main_eth.js

--- assets/js/main_eth.js
+++ assets/js/main_eth.js
@@ -169,10 +169,10 @@ function updateSellPrice(){
 					var minedAmount = web3.utils.fromWei(sun) - web3.utils.fromWei(fee);
 										
 					if(window.tokenData != null){
-						eggstoselldoc.textContent = formatTrxValue(minedAmount);
+						eggstoselldoc.textContent = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(minedAmount);
 						// sell-price-usd
 						var usd = minedAmount * window.tokenData.market_data.current_price.usd;	
-						usdPriceNode.textContent = formatTrxValue(usd);
+						usdPriceNode.textContent = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format((usd);
 					}else{
 						eggstoselldoc.textContent=formatTrxValue(minedAmount);
 					}
